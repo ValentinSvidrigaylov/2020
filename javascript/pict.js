a1a1a418cbcd3b50ef4f305b8ffe64c24308e46b
ValentinSvidrigaylov <73439701+ValentinSvidrigaylov@users.noreply.github.com>
2021-01-18 18:18:48 +0300

    Обновляет файлы сайта

---
+++ javascript/pict.js
@@ -0,0 +1,20 @@
+// JavaScript Document
+function gettrip()
+  {
+  logo.src="images/logo__active.png";
+   }
+  
+function show()
+    {
+	logo.src="images/logo.png";
+    }
+	
+function gettrip1()
+  {
+  logo.src="../images/logo__active.png";
+   }
+  
+function show1()
+    {
+	logo.src="../images/logo.png";
+    }
